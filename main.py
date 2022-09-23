b98e6196c950d78422c16a750dc4727cba703e53
cespenar <cespenar1@gmail.com>
2022-09-23 11:13:39 +0200

    Query parameters

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 from fastapi import FastAPI
-from enum import Enum
-
-
-class ModelName(str, Enum):
-    alexnet = 'alexnet'
-    resnet = 'resnet'
-    lenet = 'lenet'
-
 
 app = FastAPI()
 
 
-@app.get('/models/{model_name}')
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet:
-        return {'model_name': model_name, 'message': 'Deep learning FTW!'}
-    
-    if model_name.value == 'lenet':
-        return {'model_name': model_name, 'message': 'LeCNN all the images'}
-
-    return {'model_name': model_name, 'message': 'Have some residuals'}
+@app.get('/items/{item_id}')
+async def read_user_item(item_id: str, needy: str, skip: int = 0,
+                         limit: int | None = None):
+    item = {'item_id': item_id, 'needy': needy, 'skip': skip, 'limit': limit}
+    return item
