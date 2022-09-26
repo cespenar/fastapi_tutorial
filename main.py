0683ee7bb66401ee7ee876f9f34b945ce6c276e4
cespenar <cespenar1@gmail.com>
2022-09-26 10:14:10 +0200

    Response model

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel
 
 app = FastAPI()
-
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
 
 
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
-    tax: float | None = None
-    tags: set[str] = set()
-    images: list[Image] | None = None
+    tax: float = 10.5
 
 
-@app.put('/items/{item_id}')
-async def update_item(item_id: int, item: Item):
-    results = {'item_id': item_id, 'item': item}
-    return results
+items = {
+    'foo': {'name': 'Foo', 'price': 50.2},
+    'bar': {'name': 'Bar', 'description': 'The Bar fighters', 'price': 62,
+            'tax': 20.2},
+    'baz': {
+        'name': 'Baz',
+        'description': 'There goes my baz',
+        'price': 50.2,
+        'tax': 10.5,
+    },
+}
+
+
+@app.get(
+    '/items/{item_id}/name',
+    response_model=Item,
+    response_model_include={'name', 'description'},
+)
+async def read_item_name(item_id: str):
+    return items[item_id]
+
+
+@app.get('/items/{item_id}/public', response_model=Item,
+         response_model_exclude={'tax'})
+async def read_item_public_data(item_id: str):
+    return items[item_id]
