911cc622a488860f4b91e273a94839195d161e8d
cespenar <cespenar1@gmail.com>
2022-09-27 12:43:49 +0200

    Dependencies

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
-from fastapi import Depends, FastAPI
-
-app = FastAPI()
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 
-fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}]
+async def verify_token(x_token: str = Header()):
+    if x_token != 'fake-super-secret-token':
+        raise HTTPException(status_code=400, detail='X-Token header invalid')
 
 
-class CommonQueryParams:
-    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
-        self.q = q
-        self.skip = skip
-        self.limit = limit
+async def verify_key(x_key: str = Header()):
+    if x_key != 'fake-super-secret-key':
+        raise HTTPException(status_code=400, detail='X-Key header invalid')
+    return x_key
+
+
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
 
 
 @app.get('/items/')
-async def read_items(commons: CommonQueryParams = Depends()):
-    response = {}
-    if commons.q:
-        response.update({'q': commons.q})
-    items = fake_items_db[commons.skip : commons.skip + commons.limit]
-    response.update({'items': items})
-    return response
+async def read_items():
+    return [{'item': 'Portal Gun'}, {'item': 'Plumbus'}]
+
+
+@app.get('/users/')
+async def read_users():
+    return [{'username': 'Rick'}, {'username': 'Morty'}]
