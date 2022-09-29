55a3bd129277a902513c598ea1239543a9745629
cespenar <cespenar1@gmail.com>
2022-09-29 14:35:33 +0200

    Testing

--- main.py
+++ main.py
@@ -1,28 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Header, HTTPException
+from pydantic import BaseModel
 
-tags_metadata = [
-    {
-        'name': 'users',
-        'description': 'Operations with users. The **login** logic is also here.',
-    },
-    {
-        'name': 'items',
-        'description': 'Manage items. So _fancy_ they have their own docs.',
-        'externalDocs': {
-            'description': 'Items external docs',
-            'url': 'https://fastapi.tiangolo.com/',
-        },
-    },
-]
+fake_secret_token = 'coneofsilence'
 
-app = FastAPI(openapi_tags=tags_metadata)
+fake_db = {
+    'foo': {'id': 'foo', 'title': 'Foo', 'description': 'There goes my hero'},
+    'bar': {'id': 'bar', 'title': 'Bar', 'description': 'The bartenders'},
+}
+
+app = FastAPI()
 
 
-@app.get('/users/', tags=['users'])
-async def get_users():
-    return [{'name': 'Harry'}, {'name': 'Ron'}]
+class Item(BaseModel):
+    id: str
+    title: str
+    description: str | None = None
 
 
-@app.get('/items/', tags=['items'])
-async def get_items():
-    return [{'name': 'wand'}, {'name': 'flying broom'}]
+@app.get('/items/{item_id}', response_model=Item)
+async def read_main(item_id: str, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail='Invalid X-Token header')
+    if item_id not in fake_db:
+        raise HTTPException(status_code=404, detail='Item not found')
+    return fake_db[item_id]
+
+
+@app.post('/items/', response_model=Item)
+async def create_item(item: Item, x_token: str = Header()):
+    if x_token != fake_secret_token:
+        raise HTTPException(status_code=400, detail='Invalid X-Token header')
+    if item.id in fake_db:
+        raise HTTPException(status_code=400, detail='Item already exists')
+    fake_db[item.id] = item
+    return item
